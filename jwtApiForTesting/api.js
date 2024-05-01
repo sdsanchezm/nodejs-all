20c0b77302478746fded2535edf50f4861511fc6
sdsanchezm <sdsanchezm@gmail.com>
2024-04-30 20:04:53 -0500

    adjusting details and ready to go up for testing

--- jwtApiForTesting/api.js
+++ jwtApiForTesting/api.js
@@ -1,14 +1,21 @@
+// this node api is only for testing purposes
+// is not a real/production api
+// the idea is 
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const secretKey = 'Jenesepa426384-Jenesepa426384-Jenesepa426384-';
+// Jamecho-The-Dog-426384 -> B64
+const secretKey = 'SmFtZWNoby1UaGUtRG9nLTQyNjM4NA==';
 
+
+// hardcoded database
 const users = [
     { id: 1, username: 'user1', password: 'password1', role: 'USER' },
     { id: 2, username: 'admin', password: 'adminpassword', role: 'ADMIN' }
 ];
 
+// middleware to validate the jwt token
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
@@ -27,6 +34,12 @@ const verifyToken = (req, res, next) => {
     });
 };
 
+// login endpoing, send in the body the username and pwrt:
+// {
+//     "username": "user",
+//     "password": "password1"
+// }
+// the jwt token generated, will have to be sent as a Bearer token
 app.post('/api/login', express.json(), (req, res) => {
     const { username, password } = req.body;
 
@@ -41,25 +54,29 @@ app.post('/api/login', express.json(), (req, res) => {
     res.json({ token });
 });
 
+// general endpoint
 app.get('/api/common', verifyToken, (req, res) => {
-    res.json({ message: 'Common endpoint accessed', user: req.user });
+    res.json({ message: 'endpoint /common accessed', user: req.user });
 });
 
+// user endpoint
 app.get('/api/user', verifyToken, (req, res) => {
     if (req.user.role !== 'USER') {
         return res.status(403).json({ message: 'Forbidden' });
     }
-    res.json({ message: 'User endpoint accessed', user: req.user });
+    res.json({ message: 'endpoint /user accessed', user: req.user });
 });
 
+// admin endpoint
 app.get('/api/admin', verifyToken, (req, res) => {
     if (req.user.role !== 'ADMIN') {
         return res.status(403).json({ message: 'Forbidden' });
     }
-    res.json({ message: 'Admin endpoint accessed', user: req.user });
+    res.json({ message: 'endpoint /admin accessed', user: req.user });
 });
 
+// server up!
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server up on port ${PORT}`);
 });
