f9bd2e510a20b0319962edee1d558277330b84e9
sdsanchezm <sdsanchezm@gmail.com>
2024-07-13 18:36:03 -0500

    update: minor

--- jenkinsTestingPipelinesNode/test/index.js
+++ jenkinsTestingPipelinesNode/test/index.js
@@ -5,7 +5,7 @@ const mul = require('../');
 describe('Multiplication', () => {
 
     it('positive numbers handling', () => {
-        assert.equal(mul(4, 6), 24);
+        assert.equal(mul(4, 7), 28);
     });
 
     it('negative numbers handling', () => {
